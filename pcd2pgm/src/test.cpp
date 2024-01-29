43e6af6ef3809f3bdebabe2de51e35e680d31520
eric <haiyang.wang@clobotics.com>
2024-01-29 11:58:36 +0800

    simplify code

--- pcd2pgm/src/test.cpp
+++ pcd2pgm/src/test.cpp
@@ -25,7 +25,7 @@ const std::string pcd_format = ".pcd";
 nav_msgs::OccupancyGrid map_topic_msg;
 
 double thre_z_min = 0.3;
-double thre_z_max = 2.0;
+double thre_z_max = 0.6;
 int flag_pass_through = 0;
 
 double grid_x = 0.1;
@@ -40,71 +40,69 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_after_PassThrough(new pcl::PointCloud<
 pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_after_Radius(new pcl::PointCloud<pcl::PointXYZ>);
 pcl::PointCloud<pcl::PointXYZ>::Ptr pcd_cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
-void PassThroughFilter(const double& thre_low, const double& thre_high, const bool& flag_in);
+void PassThroughFilter(const double &thre_low, const double &thre_high, const bool &flag_in);
 
-void RadiusOutlierFilter(const pcl::PointCloud<pcl::PointXYZ>::Ptr& pcd_cloud, const double &radius, const int &thre_count);
+void
+RadiusOutlierFilter(const pcl::PointCloud<pcl::PointXYZ>::Ptr &pcd_cloud, const double &radius, const int &thre_count);
 
-void SetMapTopicMsg(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, nav_msgs::OccupancyGrid& msg);
+void SetMapTopicMsg(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, nav_msgs::OccupancyGrid &msg);
 
-int main(int argc, char** argv)
-{
-   ros::init(argc, argv, "pcl_filters");
-   ros::NodeHandle nh;
-   ros::NodeHandle private_nh("~");
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "pcl_filters");
+    ros::NodeHandle nh;
+    ros::NodeHandle private_nh("~");
 
-   ros::Rate loop_rate(1.0);
+    ros::Rate loop_rate(1.0);
 
-   private_nh.param("file_directory", file_directory, std::string("/home/ubuntu/"));
-   ROS_INFO("*** file_directory = %s ***\n", file_directory.c_str());
+    private_nh.param("file_directory", file_directory,
+                     std::string("/home/eric/ws_fast_lio2/src/FAST_LIO_ROS2/FAST_LIO/PCD/"));
+    ROS_INFO("*** file_directory = %s ***\n", file_directory.c_str());
 
 
-   private_nh.param("file_name", file_name, std::string("good2"));
-   ROS_INFO("*** file_name = %s ***\n", file_name.c_str());
+    private_nh.param("file_name", file_name, std::string("result"));
+    ROS_INFO("*** file_name = %s ***\n", file_name.c_str());
 
-   pcd_file = file_directory + file_name + pcd_format;
-   ROS_INFO("*** pcd_file = %s ***\n", pcd_file.c_str());
+    pcd_file = file_directory + file_name + pcd_format;
+    ROS_INFO("*** pcd_file = %s ***\n", pcd_file.c_str());
 
-   private_nh.param("thre_z_min", thre_z_min, 0.5);
-   private_nh.param("thre_z_max", thre_z_max, 2.0);
-   private_nh.param("flag_pass_through", flag_pass_through, 0);
-   private_nh.param("grid_x", grid_x, 0.1);
-   private_nh.param("grid_y", grid_y, 0.1);
-   private_nh.param("grid_z", grid_z, 0.1);
-   private_nh.param("thre_radius", thre_radius, 0.5);
-   private_nh.param("map_resolution", map_resolution, 0.05);
-   private_nh.param("map_topic_name", map_topic_name, std::string("map"));
+    private_nh.param("thre_z_min", thre_z_min, 0.5);
+    private_nh.param("thre_z_max", thre_z_max, 2.0);
+    private_nh.param("flag_pass_through", flag_pass_through, 0);
+    private_nh.param("grid_x", grid_x, 0.1);
+    private_nh.param("grid_y", grid_y, 0.1);
+    private_nh.param("grid_z", grid_z, 0.1);
+    private_nh.param("thre_radius", thre_radius, 0.5);
+    private_nh.param("map_resolution", map_resolution, 0.05);
+    private_nh.param("map_topic_name", map_topic_name, std::string("map"));
 
-   ros::Publisher map_topic_pub = nh.advertise<nav_msgs::OccupancyGrid>(map_topic_name, 1);
+    ros::Publisher map_topic_pub = nh.advertise<nav_msgs::OccupancyGrid>(map_topic_name, 1);
 
-   if (pcl::io::loadPCDFile<pcl::PointXYZ> (pcd_file, *pcd_cloud) == -1)
-   {
-     PCL_ERROR ("Couldn't read file: %s \n", pcd_file.c_str());
-     return (-1);
-   }
+    if (pcl::io::loadPCDFile<pcl::PointXYZ>(pcd_file, *pcd_cloud) == -1) {
+        PCL_ERROR ("Couldn't read file: %s \n", pcd_file.c_str());
+        return (-1);
+    }
 
-   std::cout << "初始点云数据点数：" << pcd_cloud->points.size() << std::endl;
+    std::cout << "初始点云数据点数：" << pcd_cloud->points.size() << std::endl;
 
-   PassThroughFilter(thre_z_min, thre_z_max, bool(flag_pass_through));
+    PassThroughFilter(thre_z_min, thre_z_max, bool(flag_pass_through));
 
 //   RadiusOutlierFilter(cloud_after_PassThrough, 0.1, 10);
 //   SetMapTopicMsg(cloud_after_Radius, map_topic_msg);
 
-   SetMapTopicMsg(cloud_after_PassThrough, map_topic_msg);
+    SetMapTopicMsg(cloud_after_PassThrough, map_topic_msg);
 
-   while(ros::ok())
-   {
-     map_topic_pub.publish(map_topic_msg);
+    while (ros::ok()) {
+        map_topic_pub.publish(map_topic_msg);
 
-     loop_rate.sleep();
+        loop_rate.sleep();
 
-     ros::spinOnce();
-   }
+        ros::spinOnce();
+    }
 
-   return 0;
+    return 0;
 }
 
-void PassThroughFilter(const double &thre_low, const double &thre_high, const bool &flag_in)
-{
+void PassThroughFilter(const double &thre_low, const double &thre_high, const bool &flag_in) {
     /*方法一：直通滤波器对点云进行处理。*/
     pcl::PassThrough<pcl::PointXYZ> passthrough;
     passthrough.setInputCloud(pcd_cloud);//输入点云
@@ -115,8 +113,8 @@ void PassThroughFilter(const double &thre_low, const double &thre_high, const bo
     std::cout << "直通滤波后点云数据点数：" << cloud_after_PassThrough->points.size() << std::endl;
 }
 
-void RadiusOutlierFilter(const pcl::PointCloud<pcl::PointXYZ>::Ptr& pcd_cloud0, const double &radius, const int &thre_count)
-{
+void RadiusOutlierFilter(const pcl::PointCloud<pcl::PointXYZ>::Ptr &pcd_cloud0, const double &radius,
+                         const int &thre_count) {
     pcl::RadiusOutlierRemoval<pcl::PointXYZ> radiusoutlier;  //创建滤波器
 
     radiusoutlier.setInputCloud(pcd_cloud0);    //设置输入点云
@@ -127,73 +125,68 @@ void RadiusOutlierFilter(const pcl::PointCloud<pcl::PointXYZ>::Ptr& pcd_cloud0, 
     std::cout << "半径滤波后点云数据点数：" << cloud_after_Radius->points.size() << std::endl;
 }
 
-void SetMapTopicMsg(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, nav_msgs::OccupancyGrid& msg)
-{
-  msg.header.seq = 0;
-  msg.header.stamp = ros::Time::now();
-  msg.header.frame_id = "map";
+void SetMapTopicMsg(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, nav_msgs::OccupancyGrid &msg) {
+    msg.header.seq = 0;
+    msg.header.stamp = ros::Time::now();
+    msg.header.frame_id = "map";
 
-  msg.info.map_load_time = ros::Time::now();
-  msg.info.resolution = map_resolution;
+    msg.info.map_load_time = ros::Time::now();
+    msg.info.resolution = map_resolution;
 
-  double x_min, x_max, y_min, y_max;
-  double z_max_grey_rate = 0.05;
-  double z_min_grey_rate = 0.95;
-  double k_line = (z_max_grey_rate - z_min_grey_rate) / (thre_z_max - thre_z_min);
-  double b_line = (thre_z_max * z_min_grey_rate - thre_z_min * z_max_grey_rate) / (thre_z_max - thre_z_min);
+    double x_min, x_max, y_min, y_max;
+    double z_max_grey_rate = 0.05;
+    double z_min_grey_rate = 0.95;
+    double k_line = (z_max_grey_rate - z_min_grey_rate) / (thre_z_max - thre_z_min);
+    double b_line = (thre_z_max * z_min_grey_rate - thre_z_min * z_max_grey_rate) / (thre_z_max - thre_z_min);
 
-  if(cloud->points.empty())
-  {
-    ROS_WARN("pcd is empty!\n");
+    if (cloud->points.empty()) {
+        ROS_WARN("pcd is empty!\n");
 
-    return;
-  }
-
-  for(int i = 0; i < cloud->points.size() - 1; i++)
-  {
-    if(i == 0)
-    {
-      x_min = x_max = cloud->points[i].x;
-      y_min = y_max = cloud->points[i].y;
+        return;
     }
 
-    double x = cloud->points[i].x;
-    double y = cloud->points[i].y;
+    for (int i = 0; i < cloud->points.size() - 1; i++) {
+        if (i == 0) {
+            x_min = x_max = cloud->points[i].x;
+            y_min = y_max = cloud->points[i].y;
+        }
 
-    if(x < x_min) x_min = x;
-    if(x > x_max) x_max = x;
+        double x = cloud->points[i].x;
+        double y = cloud->points[i].y;
 
-    if(y < y_min) y_min = y;
-    if(y > y_max) y_max = y;
-  }
+        if (x < x_min) x_min = x;
+        if (x > x_max) x_max = x;
 
-  msg.info.origin.position.x = x_min;
-  msg.info.origin.position.y = y_min;
-  msg.info.origin.position.z = 0.0;
-  msg.info.origin.orientation.x = 0.0;
-  msg.info.origin.orientation.y = 0.0;
-  msg.info.origin.orientation.z = 0.0;
-  msg.info.origin.orientation.w = 1.0;
+        if (y < y_min) y_min = y;
+        if (y > y_max) y_max = y;
+    }
 
-  msg.info.width = int((x_max - x_min) / map_resolution);
-  msg.info.height = int((y_max - y_min) / map_resolution);
+    msg.info.origin.position.x = x_min;
+    msg.info.origin.position.y = y_min;
+    msg.info.origin.position.z = 0.0;
+    msg.info.origin.orientation.x = 0.0;
+    msg.info.origin.orientation.y = 0.0;
+    msg.info.origin.orientation.z = 0.0;
+    msg.info.origin.orientation.w = 1.0;
 
-  msg.data.resize(msg.info.width * msg.info.height);
-  msg.data.assign(msg.info.width * msg.info.height, 0);
+    msg.info.width = int((x_max - x_min) / map_resolution);
+    msg.info.height = int((y_max - y_min) / map_resolution);
 
-  ROS_INFO("data size = %d\n", msg.data.size());
+    msg.data.resize(msg.info.width * msg.info.height);
+    msg.data.assign(msg.info.width * msg.info.height, 0);
 
-  for(int iter = 0; iter < cloud->points.size(); iter++)
-  {
-    int i = int((cloud->points[iter].x - x_min) / map_resolution);
-    if(i < 0 || i >= msg.info.width) continue;
+    ROS_INFO("data size = %d\n", (int) msg.data.size());
 
-    int j = int((cloud->points[iter].y - y_min) / map_resolution);
-    if(j < 0 || j >= msg.info.height - 1) continue;
+    for (int iter = 0; iter < cloud->points.size(); iter++) {
+        int i = int((cloud->points[iter].x - x_min) / map_resolution);
+        if (i < 0 || i >= msg.info.width) continue;
 
-    msg.data[i + j * msg.info.width] = 100;
+        int j = int((cloud->points[iter].y - y_min) / map_resolution);
+        if (j < 0 || j >= msg.info.height - 1) continue;
+
+        msg.data[i + j * msg.info.width] = 100;
 //    msg.data[i + j * msg.info.width] = int(255 * (cloud->points[iter].z * k_line + b_line)) % 255;
-  }
+    }
 }
 
 
